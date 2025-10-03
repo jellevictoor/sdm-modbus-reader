506b63d108334b15049a790ec657b8118f7d4092
Jelle Victoor <jelle@victoor.io>
2025-10-03 18:48:44 +0200

    corrected registers

--- sdm_modbus_reader/domain/register_maps.py
+++ sdm_modbus_reader/domain/register_maps.py
@@ -6,20 +6,20 @@ These are Modbus register addresses for reading IEEE754 float32 values.
 """
 
 SDM120_REGISTERS = {
-    "Voltage": 0x0000,
-    "Current": 0x0006,
-    "Power": 0x000C,
-    "ApparentPower": 0x0012,
-    "ReactivePower": 0x0018,
-    "Cosphi": 0x001E,
-    "PhaseAngle": 0x0024,
-    "Frequency": 0x0046,
-    "Import": 0x0156,
-    "Export": 0x0160,
-    "ReactiveImport": 0x0158,
-    "ReactiveExport": 0x0162,
-    "Sum": 0x0156,
-    "ReactiveSum": 0x0158,
+    "Voltage": 0x0000,          # 30001
+    "Current": 0x0006,          # 30007
+    "Power": 0x000C,            # 30013
+    "ApparentPower": 0x0012,    # 30019
+    "ReactivePower": 0x0018,    # 30025
+    "Cosphi": 0x001E,           # 30031
+    "PhaseAngle": 0x0024,       # 30037
+    "Frequency": 0x0046,        # 30071
+    "Import": 0x0048,           # 30073 - Import active energy
+    "Export": 0x004A,           # 30075 - Export active energy
+    "ReactiveImport": 0x004C,   # 30077 - Import reactive energy
+    "ReactiveExport": 0x004E,   # 30079 - Export reactive energy
+    "Sum": 0x0156,              # 30343 - Total active energy
+    "ReactiveSum": 0x0158,      # 30345 - Total reactive energy
 }
 
 SDM630_REGISTERS = {
